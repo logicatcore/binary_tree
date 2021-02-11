ff2d16e08fb5da21e1de9b87a55564d456c18efa
sai <kakubalsai@gmail.com>
2021-02-11 16:13:17 +0100

    add elements attribute to the binary tree class

--- python/tests/test_bt_attributes.py
+++ python/tests/test_bt_attributes.py
@@ -36,3 +36,15 @@ class TestBinaryTreeAttributes:
         bt = BT([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
         assert 5 == bt.nodes, "Number of nodes differs from the expected value"
         assert 5 == bt.leafs, "Number of leafs differs from the expected value"
+
+    def test_conversion_to_elements(self):
+        """
+        Binary tree can be created in two different ways, one by supplying a Node object, second by supplying a list of
+        elements. This test case checks the 'elements' attribute of the binary tree
+        :return: None
+        """
+        x = BT(Node(1, Node(2, Node(4), Node(5)), Node(3, Node(6), Node(7))))
+        assert [1, 2, 3, 4, 6, 5, 7] == x.elements, "The elements of the binary tree do not match"
+
+        x = BT(list(range(10)))
+        assert list(range(10)) == x.elements, "The elements of the binary tree do not match"
